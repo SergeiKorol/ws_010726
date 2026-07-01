453c5552bfe2ac59a3d717399b90e96fcfd53fa3
Татьяна <woodtv2014@gmail.com>
2026-07-02 00:07:10 +0700

    Add_new_task

---
+++ test/add_new_test.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def test_add():
+    body = {"title": "generated", "completed": False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+
+    body = {"completed": True}
+    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+    response_body = response.json()
+
+    assert response.status_code == 200
+    assert response_body['completed'] == True
